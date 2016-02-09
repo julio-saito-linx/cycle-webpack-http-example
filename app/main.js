87b46cfa42ed1a87b1f655e0ef03bfc33ce7ff56
Julio Makdisse Saito <saitodisse@gmail.com>
2016-02-08 22:19:50 -0200

    tweet with first photo

--- app/main.js
+++ app/main.js
@@ -1,13 +1,22 @@
+import Rx from 'rx';
 import Cycle from '@cycle/core';
-import {div, p, br, a, hr, makeDOMDriver} from '@cycle/dom';
+import {div, p, a, hr, img, makeDOMDriver} from '@cycle/dom';
 import FirebaseDriver from './firebase-driver';
 import './style.css';
 
 function main(sources) {
-  const allTweets$ = sources.Firebase
-    .startWith(null);
-
   const tweetList = [];
+
+  const allTweets$ = Rx.Observable.of(require('../fixtures/data.json'));
+  // const allTweets$ = sources.Firebase.startWith(null);
+
+  function getExtendedImages(tweet) {
+    if (tweet.extended_entities && tweet.extended_entities.media && tweet.extended_entities.media.length > 0) {
+      return img({src: tweet.extended_entities.media[0].media_url});
+    }
+
+    return null;
+  }
 
   function addTweet(tweet) {
     if (!tweet) {
@@ -16,18 +25,27 @@ function main(sources) {
 
     tweetList.push(
       div('.col-md-12', [
-        p('.tweet-name', [
+        div({
+          className: 'tweet-name',
+          style: {'background': `url(${tweet.user.profile_background_image_url}) right center / cover repeat scroll padding-box padding-box transparent   `},
+        }, [
+          img({src: tweet.user.profile_image_url}),
+          p('.tweet-user-name', tweet.user.name),
+          p(`${tweet.user.followers_count} (${tweet.user.friends_count}) ${tweet.user.lang}`),
           a({
-            href: `${tweet.link}`,
+            href: `https://twitter.com/${tweet.user.screen_name}`,
             target: '_blank',
             className: 'tweet-username',
-          }, tweet.user),
-          a('.tweet-website', {
-            href: `${tweet.link}`,
+            title: tweet.user.description
+          }, `@${tweet.user.screen_name}`),
+          a({
+            href: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
             target: '_blank',
-          }, 'link'),
+            className: 'tweet-link',
+          }, `${new Date(tweet.created_at).toLocaleTimeString() }`),
+          p('.tweet-text', tweet.text),
+          getExtendedImages(tweet),
         ]),
-        p('.tweet-text', tweet.text.replace(/\n/g, '<br>')),
         hr(),
       ])
     );
